49c640b4ccd6849189f4ee799d4ba9d2497ce6da
Raúl Llano <rllano001@ikasle.ehu.eus>
2016-11-25 18:07:26 +0100

    Pagina de Registro de producto validada con javaScript

--- public_html/JavaScript/paginaRegistroProducto.js
+++ public_html/JavaScript/paginaRegistroProducto.js
@@ -3,9 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-window.onload = startDB;
+window.onload = empieza;
 var indexedDB = window.indexedDB || window.webkitIndexedDB;
 var dataBase = null;
+
+function empieza(){
+    startDB();
+    revisar();
+    
+}
 
 function startDB(){
     
@@ -145,3 +151,81 @@ function login(){
     }
     
 }
+
+function revisar(){
+    document.getElementById("btnRegistroProducto").addEventListener("click", revisionGeneral, false);
+    var nombre = document.getElementById("nombreproducto");
+    nombre.oninput = function() {
+        if(!revisarNombre())
+            nombre.className='error';
+        else
+            nombre.className='form-input';
+    }
+    var descripcion = document.getElementById("descripcion");
+    descripcion.oninput = function() {
+        if(!revisarDescripcion())
+            descripcion.className='error';
+        else
+            descripcion.className='form-input';
+    }
+    var precio = document.getElementById("precio");
+    precio.oninput = function() {
+        if(revisarPrecio())
+            precio.className='error';
+        else
+            precio.className='form-input';
+    }
+    
+    
+}
+function revisionGeneral() {
+    var correcto = true;
+    var nombre = document.getElementById("nombreproducto");
+    if(!revisarNombre()){
+        correcto=false;
+        nombre.className='error';
+    }
+    var descripcion = document.getElementById("descripcion");
+    if(!revisarDescripcion()){
+        correcto=false;
+        descripcion.className='error';
+    }
+    var precio = document.getElementById("precio");
+    if(!revisarPrecio()){
+        correcto=false;
+        precio.className='error';
+    }
+    
+    if(correcto){
+        document.getElementById("formRegProducto").submit();
+        document.location.href = "http://localhost:8383/HTMLVibbay08/public_html/index.html";
+        //alert("11");
+    }
+    }
+    
+function revisarNombre() {
+    var nombre = document.getElementById("nombreproducto");
+    var ex = /[A-Za-z]{3,}/;
+    if(!ex.test(nombre.value) || nombre.value==="")
+        return false;
+    else
+        return true;
+}
+function revisarDescripcion() {
+    var descripcion = document.getElementById("descripcion");
+    var ex = /^[\s\S]{10,20}$/;
+    if(!ex.test(descripcion.value) || descripcion.value==="")
+        return false;
+    else
+        return true;
+}
+
+function revisarPrecio() {
+    var precio = document.getElementById("precio");
+    var ex =/^[10-1000]+$/;
+    if(!ex.test(precio.value) || precio.value==="")
+        return false;
+    else
+        return true;
+}
+
